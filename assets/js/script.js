c569511b66cc39b78d01a1b454165c3cbd15640a
Emily <emilys@genstudents.org>
2025-02-11 12:52:28 -0300

    test: Alterações

---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+const about = document.querySelector('#about');
+const formulario = document.querySelector('#formulario');
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+async function getApiGithub() {
+
+    try {
+        const response = await fetch(`https://api.github.com/users/emilyestvz`);
+        const perfil = await response.json();
+        
+        about.innerHTML = `
+            <article id="about-img" class="about-content">
+          
+                <img src="${perfil.avatar_url}" alt="${perfil.login}">
+                </article>
+
+                <article id="about-txt" class="flex home-content">
+
+                <h1>SOBRE MIM</h1>
+                <p>Atualmente em transição de carreira, venho de uma experiência enriquecedora na Espanha de 2022 a 2023, onde estudei e alcancei fluência em espanhol, além de aprimorar meu inglês e adquirir uma bagagem cultural valiosa.
+                <br>Anteriormente, trabalhei com atendimento ao cliente e vendas em vários âmbitos, onde tive a oportunidade de aprimorar minha adaptabilidade, comunicação, trabalho em equipe e metodologias ágeis.</p>
+
+                <div id="about-github">
+                    <a class="botao" href="https://github.com/emilyestvz" target="_blank">
+                    GitHub
+                    </a>
+                </div>
+
+                <div>
+                    <p>
+                    <img src="./assets/icons/user.svg" alt="Ícone user">${perfil.followers} Seguidores
+                    <br>
+                    <img src="./assets/icons/folder.svg" alt="Ícone folder">${perfil.public_repos} Repositórios
+                </p>
+                </div>
+        </article>
+            `
+            about.innerHTML += conteudo;
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+formulario.addEventListener('submit', (event) => {
+
+    // Após passar pelas validações, envia o email
+    event.preventDefault();
+
+    let formValido = true;
+
+    // Validação nome
+    const campoNome = document.querySelector('#nome');
+    const txtNome = document.querySelector('#txtNome');
+
+    if(campoNome.value.length < 3) {
+        txtNome.innerHTML = `O nome deve ter no mínimo 3 caracteres.`
+        campoNome.focus()
+        formValido = false;
+    }else{ 
+        txtNome.innerHTML = '';
+    }
+
+    // Validação email
+    const campoEmail = document.querySelector('#email');
+    const txtEmail = document.querySelector('#txtEmail');
+
+    if(!emailRegex.value.match(campoEmail)) {
+        txtEmail.innerHTML = `O email digitado é inválido.`
+        campoEmail.focus()
+        formValido = false;
+    } else{
+        txtEmail.innerHTML = '';
+    }
+
+    // Validação assunto
+    const campoAssunto = document.querySelector('#assunto');
+    const txtAssunto = document.querySelector('#txtAssunto');
+
+    if(campoAssunto.value.length < 5) {
+        txtAssunto.innerHTML = `O assunto deve conter no mínimo 5 caracteres.`
+        campoAssunto.focus()
+        formValido = false;
+    }else{ 
+        txtAssunto.innerHTML = '';
+    }
+
+    // Se tudo estiver válido, envia o formulário
+    if (formValido) {
+        alert('Formulário enviado com sucesso! ✨');
+        formulario.submit(); 
+        formulario.reset();
+    }
+})
+
+getApiGithub();
